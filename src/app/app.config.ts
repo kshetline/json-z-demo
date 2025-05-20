30e358f16dd35db5b0d5416f775d7407eb5ce972
Kerry Shetline <kerry@shetline.com>
2025-05-20 18:18:15 -0400

    Fix tooltip styling.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,35 +2,20 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import {palette} from '@primeng/themes';
 import { provideHttpClient } from '@angular/common/http';
 import { definePreset } from '@primeng/themes';
 
 const AuraSky = definePreset(Aura, {
   semantic: {
-    primary: {
-      50: '{sky.50}',
-      100: '{sky.100}',
-      200: '{sky.200}',
-      300: '{sky.300}',
-      400: '{sky.400}',
-      500: '{sky.500}',
-      600: '{sky.600}',
-      700: '{sky.700}',
-      800: '{sky.800}',
-      900: '{sky.900}',
-      950: '{sky.950}'
-    }
+    primary: palette('{sky}')
   }
 });
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: AuraSky
-      }
-    }),
+    providePrimeNG({ theme: { preset: AuraSky } }),
     provideHttpClient()
   ]
 };
